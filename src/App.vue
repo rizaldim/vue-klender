8273ff360d596ee6f9830f995e7d02e9cbd4c51b
Rizaldi Martaputra <rizaldi.m@gmail.com>
2019-07-06 02:36:38 +0700

    New component DatePicker with dummy content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<date-picker />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DatePicker from './components/DatePicker'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		DatePicker
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/DatePicker.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="calendar">
+		<div class="month-indicator">July 2019</div>
+		<div class="day-of-week">
+			<div>Su</div>
+			<div>Mo</div>
+			<div>Tu</div>
+			<div>We</div>
+			<div>Th</div>
+			<div>Fr</div>
+			<div>Sa</div>
+		</div>
+		<div class="date-grid">
+			<button v-for="index in 30" :key="index+1">{{ index }}</button>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+.calendar {
+	width: 640px;
+}
+.day-of-week, .date-grid {
+	display: grid;
+	grid-template-columns: repeat(7, 1fr);
+}
+.day-of-week {
+	height: 40px;
+}
+.date-grid button {
+	height: 40px;
+}
+</style>
