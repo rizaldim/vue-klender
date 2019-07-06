8861a96d12ec7cd2b31bb0ba5cefeeef73da2e05
Rizaldi Martaputra <rizaldi.m@gmail.com>
2019-07-07 01:39:57 +0700

    Show date based on current month

---
+++ src/components/DateCell.vue
@@ -0,0 +1,25 @@
+<template>
+	<button>
+		<time :datetime="dateTime">{{ date }}</time>
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'DateCell',
+	props: {
+		dateTime: {
+			type: String,
+			required: true
+		},
+		date: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,27 +1,67 @@
 <template>
 	<div class="calendar">
-		<div class="month-indicator">July 2019</div>
+		<div class="month-indicator">{{ monthString() }}</div>
 		<div class="day-of-week">
-			<div>Su</div>
-			<div>Mo</div>
-			<div>Tu</div>
-			<div>We</div>
-			<div>Th</div>
-			<div>Fr</div>
-			<div>Sa</div>
+			<div v-for="name in dayNames()" :key="name">{{ name }}</div>
 		</div>
 		<div class="date-grid">
-			<button v-for="index in 30" :key="index+1">{{ index }}</button>
+			<date-cell v-for="cell in cells"
+				:key="cell.dateTime"
+				:dateTime="cell.dateTime"
+				:date="cell.date"
+				/>
 		</div>
 	</div>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+import DateCell from './DateCell'
+
+export default {
+	components: {
+		DateCell
+	},
+	data() {
+		return {
+		}
+	},
+	computed: {
+		cells() {
+			const now = dayjs()
+			const startOfWeekOfStartOfMonth = now.startOf('month').startOf('week')
+			const endOfWeekOfEndOfMonth = now.endOf('month').endOf('week')
+			const cells = []
+			var day = startOfWeekOfStartOfMonth
+			while (day.diff(endOfWeekOfEndOfMonth) < 0) {
+				cells.push({
+					dateTime: day.format('YYYY-MM-DD'),
+					date: day.format('D')
+				})
+				day = day.add(1, 'day')
+			}
+			return cells
+		}
+	},
+	methods: {
+		monthString() {
+			return dayjs().format('MMMM YYYY')
+		},
+		dayNames() {
+			var names = []
+			const startOfWeek = dayjs().startOf('week')
+			for (var i = 0; i < 7; i++) {
+				names.push(startOfWeek.add(i, 'day').format('dd'))
+			}
+			return names
+		}
+	}
+}
 </script>
 
 <style>
 .calendar {
-	width: 640px;
+	width: 300px;
 }
 .day-of-week, .date-grid {
 	display: grid;
